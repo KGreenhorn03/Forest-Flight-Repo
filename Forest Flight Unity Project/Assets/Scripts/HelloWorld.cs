9f3313665ffb921ea588cd086158a5ce5d88ed54
KGreenhorn03 <kylegreenhorn@hotmail.co.uk>
2022-03-10 10:48:34 +0000

    Created Unity Project and added HelloWorld script.

---
+++ Forest Flight Unity Project/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,47 @@
+//---------------------------------------------------------------
+// HelloWorld.
+// Coded by Kyle Greenhorn.
+// 10/03/2022.
+//---------------------------------------------------------------
+
+
+
+//---------------------------------------------------------------
+//Libraries used.
+//---------------------------------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//---------------------------------------------------------------
+
+// Class HelloWorld.
+// Class Contains everything in the script.
+
+//---------------------------------------------------------------
+public class HelloWorld : MonoBehaviour
+{
+
+    //---------------------------------------------------------------
+    // Start is called before the first frame update.
+    // Called automatically by Unity.
+    void Start()
+    {
+
+        Debug.LogWarning("Fortnite battle pass");
+
+    }
+    //---------------------------------------------------------------
+
+
+
+    //---------------------------------------------------------------
+    // Update is called once per frame.
+    // Called automatically by Unity.
+    void Update()
+    {
+
+        Debug.LogError("Max Verstappen, Champion of the World!");
+
+    }
+    //---------------------------------------------------------------
+}
