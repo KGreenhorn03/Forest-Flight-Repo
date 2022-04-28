df59ed60846cd9925d1a6264a4479c3fb7158ba8
KGreenhorn03 <kylegreenhorn@hotmail.co.uk>
2022-04-28 10:57:24 +0100

    Tilemap lesson

---
+++ Forest Flight Unity Project/Assets/Scripts/Door.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+
+    public string levelName;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.CompareTag("Player"))
+        {
+
+            SceneManager.LoadScene(levelName);
+
+        }
+
+    }
+
+}
