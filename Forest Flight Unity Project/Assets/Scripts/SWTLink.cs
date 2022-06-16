3f13e8d56eb2c78c4aa4e07be8f5156d3942f39c
KGreenhorn03 <kylegreenhorn@hotmail.co.uk>
2022-06-16 10:55:36 +0100

    Added Scottish Wildlife Trust Button.

---
+++ Forest Flight Unity Project/Assets/Scripts/SWTLink.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SWTLink : MonoBehaviour
+{
+
+    public void SWT()
+    {
+
+        Application.OpenURL("https://scottishwildlifetrust.org.uk/");
+
+    }
+
+}
